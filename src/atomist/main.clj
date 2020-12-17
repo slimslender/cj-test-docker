78f0731bfb445eae5472c7f2d17790884ad4ac28
Jim Clark <jim@atomist.com>
2020-12-16 22:08:43 -0800

    Should be a good test docker container

---
+++ src/atomist/main.clj
@@ -0,0 +1,12 @@
+(ns atomist.main
+  (:require [ring.adapter.jetty :refer [run-jetty]])
+  (:gen-class)
+  )
+
+(defn handler [_]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello"})
+
+(defn -main [& args]
+  (run-jetty handler {:port 4000}))
