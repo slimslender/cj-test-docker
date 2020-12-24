26bfb3d4c56213b5769d8cf2fb1c4b523fab00ea
Jim Clark <jim@atomist.com>
2020-12-23 20:23:47 -0800

    bump

--- src/atomist/main.clj
+++ src/atomist/main.clj
@@ -5,7 +5,7 @@
 (defn handler [_]
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body "Hello123456789"})
+   :body "Hello1234567890"})
 
 (defn -main [& args]
   (run-jetty handler {:port 4000}))
