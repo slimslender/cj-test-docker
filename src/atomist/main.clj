1d388ae20f6a64ef07600463b5b9ada826a8f68b
Jim Clark <slimslenderslacks@gmail.com>
2021-01-18 22:43:41 -0800

    Update main.clj

--- src/atomist/main.clj
+++ src/atomist/main.clj
@@ -5,7 +5,7 @@
 (defn handler [_]
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body "Helloo1234567"})
+   :body "Helloo12345678"})
 
 (defn -main [& args]
   (run-jetty handler {:port 4000}))
